12a0cb69102679516fe33c2ed8e3b0c8917a09eb
Dishant kumawat <dishantk9799@gmail.com>
2026-05-29 17:17:20 +0530

    E-commerce config db

--- E-commerce/server.js
+++ E-commerce/server.js
@@ -1,9 +1,12 @@
 import app from './src/app.js';
 import dotenv from 'dotenv';
+import connectDB from './src/config/db.js';
 
 dotenv.config();
 
 const port = process.env.PORT || 4000;
+
+connectDB();
 
 app.listen(port, () => {
     console.log(`✅ Server is connnected on: http://localhost:${port}`);
