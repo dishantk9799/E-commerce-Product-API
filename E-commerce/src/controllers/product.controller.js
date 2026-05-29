7cebec0266922a8e3574290cd60aeda457c43519
Dishant kumawat <dishantk9799@gmail.com>
2026-05-29 21:05:10 +0530

    E-commerce add createProduct in controllers

--- E-commerce/src/controllers/product.controller.js
+++ E-commerce/src/controllers/product.controller.js
@@ -32,7 +32,7 @@ export const getProductById = asyncHandler(async (req, res) => {
 
   // ---- Find product in MongoDB ----
   const product = await Product.findById(id);
- 
+
   // ---- Throw if product not found in MongoDB ----
   if (!product) {
     throw new ApiError(404, "Product not found");
@@ -40,4 +40,34 @@ export const getProductById = asyncHandler(async (req, res) => {
 
   // ---- Success response send to client ----
   return res.status(200).json(new ApiResponse(200, product, "Product fetched successfully"));
+});
+
+
+// ---- Create product ----
+export const createProduct = asyncHandler(async (req, res) => {
+
+  // ---- Client side data ----
+  const { name, description, price, category } = req.body;
+
+  // ---- Validation ----
+  if (!name) throw new ApiError(400, "Product name is required");
+  if (!price) throw new ApiError(400, "Product price is required");
+  if (isNaN(price)) throw new ApiError(400, "Price must be a number");
+
+  // ---- Store multiple image paths ----
+  const images = [];
+
+  if (req.files) req.files.forEach((file) => images.push(file.path));
+
+  // ---- Create product in MongoDB ----
+  const product = await Product.create({
+    name,
+    description,
+    price,
+    category,
+    images
+  });
+
+  // ---- Success response send to client ----
+  return res.status(201).json(new ApiResponse(201, product, "Product created successfully"));
 });
--- E-commerce/src/routes/product.route.js
+++ E-commerce/src/routes/product.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllProducts, getProductById } from '../controllers/product.controller.js';
+import { createProduct, getAllProducts, getProductById } from '../controllers/product.controller.js';
 
 const route = express.Router();
 
 route.get('/', getAllProducts);
 router.get('/:id', getProductById);
+router.post('/', createProduct);
 
 export default route;
